b7710fcc6838aea4519578006b1d546941913177
Elena Kolevska <elena@kolevska.com>
2024-12-10 01:00:36 +0000

    remove benchmarking code
    Signed-off-by: Elena Kolevska <elena@kolevska.com>

--- publisher/main.go
+++ publisher/main.go
@@ -19,19 +19,15 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"sync"
 	"time"
 
 	dapr "github.com/dapr/go-sdk/client"
 )
 
 type benchmarkConfig struct {
-	pubsubName            string
-	topicName             string
-	numCycles             int
-	numGoRoutinesPerCycle int
-	numMessagesPerCall    int
-	totalMsgCnt           int
+	pubsubName         string
+	topicName          string
+	numMessagesPerCall int
 }
 
 func (b *benchmarkConfig) Init() error {
@@ -45,23 +41,10 @@ func (b *benchmarkConfig) Init() error {
 	}
 
 	var err error
-	b.numCycles, err = strconv.Atoi(os.Getenv("NUM_CYCLES"))
-	if err != nil {
-		return fmt.Errorf("Failed to parse NUM_CYCLES: %v", err)
-	}
-	fmt.Println("b.numCycles", b.numCycles)
-
-	b.numGoRoutinesPerCycle, err = strconv.Atoi(os.Getenv("NUM_GOROUTINES_PER_CYCLE"))
-	if err != nil {
-		return fmt.Errorf("Failed to parse NUM_GOROUTINES_PER_CYCLE: %v", err)
-	}
-
 	b.numMessagesPerCall, err = strconv.Atoi(os.Getenv("NUM_MESSAGES_PER_CALL"))
 	if err != nil {
 		return fmt.Errorf("Failed to parse NUM_MESSAGES_PER_CALL: %v", err)
 	}
-
-	b.totalMsgCnt = b.numCycles * b.numGoRoutinesPerCycle * b.numMessagesPerCall
 
 	return nil
 }
@@ -81,62 +64,33 @@ func main() {
 	}
 	defer client.Close()
 
-	if cfg.numCycles > 0 {
-		publishBenchmarkData(ctx, client, cfg)
-	} else {
-		publishDataContinuously(ctx, client, cfg)
-	}
-
-	fmt.Println("data published")
+	publishData(ctx, client, cfg)
 }
 
-func publishBenchmarkData(ctx context.Context, client dapr.Client, cfg benchmarkConfig) {
-	for i := range cfg.numCycles {
-		sendBatch(ctx, client, cfg, i)
-		fmt.Printf("Batch %d completed\n", i)
-	}
-}
-
-func publishDataContinuously(ctx context.Context, client dapr.Client, cfg benchmarkConfig) {
+func publishData(ctx context.Context, client dapr.Client, cfg benchmarkConfig) {
 	for {
-		sendBatch(ctx, client, cfg, 0)
-		time.Sleep(1 * time.Second)
-	}
-}
-
-func sendBatch(ctx context.Context, client dapr.Client, cfg benchmarkConfig, batchNumber int) {
-	var wg sync.WaitGroup
-
-	for i := range cfg.numGoRoutinesPerCycle {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-
-			var publishEventsData []interface{}
-			for j := 0; j < cfg.numMessagesPerCall; j++ {
-				message := map[string]string{
-					"id":      fmt.Sprintf("id-%d-%d-%d", batchNumber, i, j),
-					"message": fmt.Sprintf("data-%d-%d-%d", batchNumber, i, j),
-					"cnt":     strconv.Itoa(cfg.totalMsgCnt),
-				}
-
-				data, err := json.Marshal(message)
-				if err != nil {
-					fmt.Printf("Error marshalling message: %v\n", err)
-					continue
-				}
-
-				publishEventsData = append(publishEventsData, data)
+		var publishEventsData []interface{}
+		for j := 0; j < cfg.numMessagesPerCall; j++ {
+			message := map[string]string{
+				"id":      fmt.Sprintf("id-%d", j),
+				"message": fmt.Sprintf("data-%d", j),
 			}
 
-			// Publish multiple events
-			if res := client.PublishEvents(ctx, cfg.pubsubName, cfg.topicName, publishEventsData, dapr.PublishEventsWithContentType("application/json")); res.Error != nil {
-				panic(res.Error)
+			data, err := json.Marshal(message)
+			if err != nil {
+				fmt.Printf("Error marshalling message: %v\n", err)
+				continue
 			}
-			fmt.Println("Published", len(publishEventsData), "events")
-		}(i)
-	}
 
-	// Wait for all goroutines to complete
-	wg.Wait()
+			publishEventsData = append(publishEventsData, data)
+		}
+
+		// Publish multiple events
+		if res := client.PublishEvents(ctx, cfg.pubsubName, cfg.topicName, publishEventsData, dapr.PublishEventsWithContentType("application/json")); res.Error != nil {
+			panic(res.Error)
+		}
+		fmt.Println("Published", len(publishEventsData), "events")
+
+		time.Sleep(2 * time.Second)
+	}
 }
